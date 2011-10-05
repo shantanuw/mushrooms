03cc9a039619887f90840e16aa45b582ec1bd653
Shantanu Wagh <shantanuw@gmail.com>
2011-10-05 11:08:05 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mushrooms::Application.config.secret_token = 'fc0a0d58b6be1223a24ad3e92ebcafd9284afbf23eee31d7ac8ecfd742b5c3adcc3a027e8d8b990feacaca8bcbf89fd0b5d869f0dab83cdcebc1d8035415e54e'
